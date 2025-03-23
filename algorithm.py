bda740ef89a7456ceca4fbf7fc32686512f420a6
cheng <cxi@ucsb.edu>
2025-03-22 18:56:16 -0700

    testing0

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import tensorflow as tf
 from tensorflow import keras
-from keras.metrics import RootMeanSquaredError 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
@@ -17,14 +16,14 @@ tqqq.describe()
 
 #Open/Close price chart
 tqqq['date'] = pd.to_datetime(tqqq['date'])
-plt.plot(tqqq['date'], 
-        tqqq['open'], 
+plt.plot(tqqq['Date'], 
+        tqqq['Open'], 
         color="blue", 
-        label="open") 
-plt.plot(tqqq['date'], 
-        tqqq['close'], 
+        label="Open") 
+plt.plot(tqqq['Date'], 
+        tqqq['Close'], 
         color="green", 
-        label="close") 
+        label="Close") 
 plt.title("tqqq Open-Close Stock") 
 plt.legend() 
 
@@ -39,26 +38,26 @@ ss = ss.fit_transform(dataset)
 
 train_data = ss[0:int(training), :] 
 
-x_train = [] 
+x0_train = [] 
 y_train = [] 
 
 # considering 60 as the batch size, 
 # create the X_train and y_train 
 for i in range(60, len(train_data)): 
-    x_train.append(train_data[i-60:i, 0]) 
+    x0_train.append(train_data[i-60:i, 0]) 
     y_train.append(train_data[i, 0]) 
 
-x_train, y_train = np.array(x_train), np.array(y_train) 
-X_train = np.reshape(x_train, 
-                    (x_train.shape[0], 
-                    x_train.shape[1], 1)) 
+x_train, y_train = np.array(x0_train), np.array(y_train) 
+x1_train = np.reshape(x0_train, 
+                    (x0_train.shape[0], 
+                    x0_train.shape[1], 1)) 
 
 #RNN Model
 model = keras.models.Sequential()
 model.add(keras.layers.LSTM(units=64,
                             return_sequences=True,
                             input_shape
-                            =(X_train.shape[1], 1)))
+                            =(x1_train.shape[1], 1)))
 model.add(keras.layers.LSTM(units=64))
 model.add(keras.layers.Dense(128))
 model.add(keras.layers.Dropout(0.5))
@@ -69,8 +68,8 @@ print(model.summary())
 #Analyze loss & optimization
 model.compile(optimizer='adam', 
             loss='mae', 
-            metrics=[RootMeanSquaredError()]) 
-history = model.fit(X_train, y_train, 
+            metrics=[keras.metrics.RootMeanSquaredError()]) 
+history = model.fit(x1_train, y_train, 
                     epochs=20) 
 
 #Evaluate prediction
@@ -90,8 +89,8 @@ test = tqqq[training:]
 test['Predictions'] = prediction
 
 plt.figure(figsize=(10, 8))
-plt.plot(train['close'], c="b")
-plt.plot(test[['close', 'Predictions']])
+plt.plot(train['Close'], c="b")
+plt.plot(test[['Close', 'Predictions']])
 plt.title('TQQQ Stock Close Price')
 plt.ylabel("Close")
 plt.legend(['Train', 'Test', 'Predictions'])
